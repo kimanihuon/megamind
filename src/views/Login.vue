818cbdb980110c4fb92473d769ff6195da3395d2
kimanih <kimanih>
2020-03-05 23:24:44 +0300

    Login page graphics

---
+++ src/views/Login.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-container class="fill-height d-flex justify-center align-center">
+    <v-card class="mx-auto px-6 py-6" width="420" height="520">
+      <v-row no-gutters>
+        <v-col>
+          <v-card-title class="px-0 py-6">WELCOME TO WESKOOL</v-card-title>
+        </v-col>
+
+        <!-- Username -->
+        <v-col cols="12">
+          <v-text-field label="username" outlined></v-text-field>
+        </v-col>
+
+        <!-- Email -->
+        <v-col cols="12" :class=" signup ? 'email' : 'emailhide' ">
+          <v-text-field label="email" outlined type="email"></v-text-field>
+        </v-col>
+        
+        <!-- Password -->
+        <v-col cols="12" :class="signup ? 'move' : 'moveback'">
+          <v-text-field
+            label="Password"
+            outlined
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show ? 'text' : 'password'"
+            name="input-10-1"
+            hint="At least 8 characters"
+            counter
+            @click:append="show = !show"
+          ></v-text-field>
+        </v-col>
+
+        <!-- remember me -->
+        <v-col cols="6" :class="signup ? 'move' : 'moveback'">
+          <v-checkbox
+            v-model="custom"
+            label="Remember me"
+            class="pa-0 ma-0 pt-2"
+          ></v-checkbox>
+        </v-col>
+
+        <!-- Password -->
+        <v-col cols="6" align="right" :class="signup ? 'move' : 'moveback'">
+          <p class=" pa-0 ma-0 pt-2">
+            <a href="#">Forgot password</a>
+          </p>
+        </v-col>
+
+        <!-- Sign in button -->
+        <v-col cols="6" align="start" class="py-4">
+          <v-btn
+            :class=" signup ? 'move login' : 'moveback login' "
+            rounded
+            :color="signup ? 'grey' : 'blue'"
+            @click=" signup ? signup = false : '' "
+          >Sign In</v-btn>
+        </v-col>
+        
+        <!-- Signup button -->
+        <v-col cols="6" align="end" class="py-4 register">
+          <v-btn
+            :class="signup ? 'move register' : 'moveback register'"
+            rounded
+            :color="signup ? 'blue' : 'grey' "
+            @click=" signup ? '' : signup = true "
+          >Sign Up</v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      custom: true,
+      signup: false,
+      password: "",
+      show: false,
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 8 || "Min 8 characters"
+      }
+    };
+  },
+
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+$move : 0.65s;
+
+.login,
+.register {
+  -webkit-transition: background-color 1s;
+  -ms-transition: background-color 1s;
+  transition: background-color 1s;
+}
+
+.email {
+  opacity: 1;
+  transition: all $move ease-in-out;
+  transition-delay: 0.3s;
+}
+
+.emailhide {
+  opacity: 0;
+  transition: all $move ease-in-out;
+}
+
+.moveback {
+  transform: translateY(-85px);
+  transition: all $move ease-in-out;
+}
+
+.move {
+  transition: all $move ease-in-out;
+}
+</style>
