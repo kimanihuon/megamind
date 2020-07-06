4e22cd5d7ed393b2e16e1393ea401f688ce2f999
kimanih <kimanih>
2020-07-07 01:06:09 +0300

    added vue config changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-module.exports = {
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+const CompressionPlugin = require('compression-webpack-plugin')
+
+odule.exports = {
+  "configureWebpack": {
+    plugins: [new CompressionPlugin,],
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+      },
+    },
+    // plugins: [new BundleAnalyzerPlugin()]
+  },
   chainWebpack: config => {
     devServer: {
       port: 8080
