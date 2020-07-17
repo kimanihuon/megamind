82657023c0f2a36c5bc37d2d1b22c0c8fe0cd5b9
kimanih <kimanih>
2020-07-17 13:50:08 +0300

    added place holder for the tracks page

--- src/components/tracks/track.vue
+++ src/components/tracks/track.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="pa-0 ma-0 fill-height">
     <!-- If tracks exists. Else -->
-    <v-row no-gutters class="scroll" id="cscroll" v-if="tracks.length > 0">
+    <v-row no-gutters justify="center" class="scroll" id="cscroll" v-if="tracks.length > 0">
       <!-- User search dialog -->
       <v-dialog v-model="userSearchDialog" max-width="600px">
         <v-card class="px-2 pb-2">
@@ -15,59 +15,60 @@
       <v-dialog v-model="img">
         <v-img :src="this.image"></v-img>
       </v-dialog>
+      <v-col cols="6">
+        <!-- Card main card -->
+        <v-card class="pa-2 sheet" elevation="0">
+          <!-- Track header -->
 
-      <!-- Card main card -->
-      <v-card class="pa-2 sheet" elevation="0">
-        <!-- Track header -->
+          <!-- Active and archived blocks buttons -->
+          <trackstatuschips
+            :Public="true"
+            :track="column"
+            :index="index"
+            @toggle="toggle(...arguments)"
+            @share="share(...arguments)"
+          ></trackstatuschips>
 
-        <!-- Active and archived blocks buttons -->
-        <trackstatuschips
-          :Public="true"
-          :track="column"
-          :index="index"
-          @toggle="toggle(...arguments)"
-          @share="share(...arguments)"
-        ></trackstatuschips>
+          <!-- Track title -->
+          <v-row no-gutters>
+            <v-card-title class="py-0 pt-1 pb-2">{{ column.name }}</v-card-title>
+          </v-row>
 
-        <!-- Track title -->
-        <v-row no-gutters>
-          <v-card-title class="py-0 pt-1 pb-2">{{ column.name }}</v-card-title>
-        </v-row>
+          <!-- Draggable active cards -->
+          <draggable
+            v-if="column.active"
+            v-model="column.blocks"
+            v-bind="dragOptions"
+            :empty-insert-threshold="100"
+            @change="log"
+            @start="drag = true"
+            @end="drag = false"
+          >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+              <div v-for="(block, bidx) in (column.blocks)" :key="bidx+0">
+                <blocksTemplate :block="block"></blocksTemplate>
+              </div>
+            </transition-group>
+          </draggable>
 
-        <!-- Draggable active cards -->
-        <draggable
-          v-if="column.active"
-          v-model="column.blocks"
-          v-bind="dragOptions"
-          :empty-insert-threshold="100"
-          @change="log"
-          @start="drag = true"
-          @end="drag = false"
-        >
-          <transition-group type="transition" :name="!drag ? 'flip-list' : null">
-            <div v-for="(block, bidx) in (column.blocks)" :key="bidx+0">
-              <blocksTemplate :block="block"></blocksTemplate>
-            </div>
-          </transition-group>
-        </draggable>
-
-        <!-- Draggable Inactive cards -->
-        <draggable
-          v-if="!column.active"
-          v-model="column.archived"
-          v-bind="dragOptions"
-          :empty-insert-threshold="100"
-          @change="log"
-          @start="drag = true"
-          @end="drag = false"
-        >
-          <transition-group type="transition" :name="!drag ? 'flip-list' : null">
-            <div v-for="(block, bidx) in (column.archived)" :key="bidx+0">
-              <blocksTemplate :block="block"></blocksTemplate>
-            </div>
-          </transition-group>
-        </draggable>
-      </v-card>
+          <!-- Draggable Inactive cards -->
+          <draggable
+            v-if="!column.active"
+            v-model="column.archived"
+            v-bind="dragOptions"
+            :empty-insert-threshold="100"
+            @change="log"
+            @start="drag = true"
+            @end="drag = false"
+          >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+              <div v-for="(block, bidx) in (column.archived)" :key="bidx+0">
+                <blocksTemplate :block="block"></blocksTemplate>
+              </div>
+            </transition-group>
+          </draggable>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -77,7 +78,7 @@ import draggable from "vuedraggable";
 import blocksTemplate from "@/components/blocksTemplate";
 import trackstatuschips from "@/components/tracks/statusChipsPublic";
 export default {
-  props: ['column', 'index'],
+  props: ["column", "index"],
   components: {
     draggable,
     blocksTemplate,
@@ -141,7 +142,7 @@ export default {
     },
     log: function(evt) {
       window.console.log(evt);
-    },
+    }
   }
 };
 </script>
--- src/views/Tracks.vue
+++ src/views/Tracks.vue
@@ -2,16 +2,40 @@
   <v-container>
     <!-- Progress bar -->
     <progressIndicator v-if="fetching.state"></progressIndicator>
+
     <!-- Track -->
     <trackView :column="singleTrack" :index="1" v-if="!fetching.state && requestSuccess.state"></trackView>
+
     <!-- Error -->
-    <v-row v-if="!fetching.state && !requestSuccess.state" no-gutters align="center" class="fill-height">
+    <v-row
+      v-if="!fetching.state && !requestSuccess.state"
+      no-gutters
+      align="center"
+      class="fill-height"
+    >
       <v-col align="center">
         <v-card flat max-width="500" color="transparent">
           <v-card-subtitle
             class="welcome"
           >Unable to find track. Could have been made private or was deleted</v-card-subtitle>
           <v-img src="@/assets/icons/error.svg" height="200" width="200"></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <!-- No tracks -->
+    <v-row
+      v-if="!fetchingAll.state && !fetchingAllSuccess.state"
+      no-gutters
+      align="center"
+      class="fill-height"
+    >
+      <v-col align="center">
+        <v-card flat max-width="500" color="transparent">
+          <v-card-subtitle
+            class="welcome"
+          >Under construction. We're still working on this part :)</v-card-subtitle>
+          <v-img src="@/assets/icons/under-construction.svg" height="200" width="200"></v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -35,20 +59,22 @@ export default {
   },
   data() {
     return {
-      fetching: {state: true},
-      requestSuccess: { state: false},
-      single: true,
+      fetching: { state: true },
+      fetchingAll: { state: true },
+      fetchingAllSuccess: { state: false },
+      requestSuccess: { state: false },
+      single: true
     };
   },
   computed: {
-    singleTrack(){
-      return this.$store.state.publicTracks.single
+    singleTrack() {
+      return this.$store.state.publicTracks.single;
     }
   },
   mounted() {
     var instance = this;
     if (this.$route.query.id) {
-      this.single = true
+      this.single = true;
       fetchTrack(this).then(res => {
         if (res === true) {
           instance.fetching.state = false;
@@ -60,6 +86,8 @@ export default {
       });
     } else {
       fetchAllTracks(this);
+      this.fetchingAll = false;
+      this.fetchingAllSuccess = false;
     }
   }
 };
